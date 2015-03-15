ee20d8306dce10f31d02e28505d9c661ef2a713a
mhidaka <rabbitlog@gmail.com>
2015-03-15 11:24:25 +0900

    WebSocket初期コミット

--- cam/app/js/app.js
+++ cam/app/js/app.js
@@ -11,6 +11,9 @@
   var previewVideo;
   var captureBtn;
 
+  // Declare a proxy to reference the hub.
+  var chat = $.connection.chatHub;
+    
   function releaseCamera() {
     console.log('releaseCamera');
 
@@ -131,9 +134,35 @@
 
 
   function init() {
+
+    // Create a function that the hub can call to broadcast messages.
+    chat.client.broadcastMessage = function (name, message) {
+
+      // if ( message === 'takeCapure')
+      // Html encode display name and message.
+      // var encodedName = $('<div />').text(name).html();
+      // var encodedMsg = $('<div />').text(message).html();
+      // Add the message to the page.
+      //$('#discussion').append('<li><strong>' + encodedName
+      //                        + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+    };
+    
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+      // $('#sendmessage').click(function () {
+        // Call the Send method on the hub.
+        // chat.server.send($('#displayname').val(), $('#message').val());
+        // Clear text box and reset focus for next comment.
+        // $('#message').val('').focus();
+      //});
+    });
+
+
+
+
     // ストレージの取得
     //storage = navigator.getDeviceStorage('pictures');
-　　storage =navigator.getDeviceStorage("sdcard");
+　　  storage =navigator.getDeviceStorage("sdcard");
     
     
     // DOMの取得とイベント処理
